8237b201d7ab4baffbf86b4ef200e0cdd431e6c4
paul-a-cabral <73853593+paul-a-cabral@users.noreply.github.com>
2026-05-24 05:40:53 +0000

    Step 3: Update project runtime target to Java 21 - Compile: SUCCESS | Tests: SUCCESS
    Updated java.version to 21 and enabled @WebMvcTest in StudentControllerTest to restore controller test wiring.

--- src/test/java/com/example/StudentControllerTest.java
+++ src/test/java/com/example/StudentControllerTest.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import static org.mockito.BDDMockito.given;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest(StudentController.class)
+@WebMvcTest(StudentController.class)
 public class StudentControllerTest {
 
     @Autowired
